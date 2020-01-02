3cd516b564fd201a596b66903a8ea473966e9b6b
Travis McLane <tmclane@gmail.com>
2020-01-02 15:01:50 -0600

    feat: use callback func for bridge response

--- lib/interfaces/ipcmanager.go
+++ lib/interfaces/ipcmanager.go
@@ -1,9 +1,12 @@
 package interfaces
 
+// CallbackFunc is
+type CallbackFunc func(string) error
+
 // IPCManager is the event manager interface
 type IPCManager interface {
 	BindRenderer(Renderer)
-	Dispatch(message string)
+	Dispatch(message string, f CallbackFunc)
 	Start(eventManager EventManager, bindingManager BindingManager)
 	Shutdown()
 }
